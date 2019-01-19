6e0126b32468ab1904ebb473468c48aec1baf055
CodeBear <853495865@qq.com>
2019-01-19 22:33:23 +0800

    补充说明

--- spring-context/src/main/java/org/springframework/context/annotation/ComponentScanAnnotationParser.java
+++ spring-context/src/main/java/org/springframework/context/annotation/ComponentScanAnnotationParser.java
@@ -74,7 +74,10 @@ class ComponentScanAnnotationParser {
 
 
 	public Set<BeanDefinitionHolder> parse(AnnotationAttributes componentScan, final String declaringClass) {
-		//可以理解为描述扫描器的数据结构
+		//扫描器，还记不记在new AnnotationConfigApplicationContext的时候
+		//会调用AnnotationConfigApplicationContext的构造方法
+		//构造方法里面有一句 this.scanner = new ClassPathBeanDefinitionScanner(this);
+		//当时说这个对象不重要，这里就是证明了。常规用法中，实际上执行扫描的是这个scanner对象
 		ClassPathBeanDefinitionScanner scanner = new ClassPathBeanDefinitionScanner(this.registry,
 				componentScan.getBoolean("useDefaultFilters"), this.environment, this.resourceLoader);
 
